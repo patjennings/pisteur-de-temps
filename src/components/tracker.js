f5d93fb72af453d870f68f8266073ea21e9961a3
patjennings <bonjour@thomasguesnon.fr>
2019-03-27 13:02:19 +0100

    add first react component

---
+++ src/components/tracker.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Tracker extends React.Component{
+    constructor(props){
+	super(props)
+    }
+    render(){
+	return <div class="ticket">Mon ticket</div>;
+    }
+}
+
+export default Tracker;
