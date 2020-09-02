4ff1fd95467cc05664ea6f54f56f716bba505dd1
patjennings <bonjour@thomasguesnon.fr>
2020-09-02 14:17:57 +0200

    update app.js

--- api/app.js
+++ api/app.js
@@ -7,9 +7,7 @@ var mongoOp =    require("./server/models/mongo"); // le modèle mongodb
 var cors = require("cors"); // cors permet de setup les headers pour effectuer des appels cross-domain
 let API_PORT;
 
-
 var app = express();
-// const whitleListDomain = ['http://localhost:3000', 'http://localhost', 'https://time.api.thomasguesnon.net'];
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'pug');
@@ -20,21 +18,21 @@ app.use(cors());
 
 
 var sess = {
-  secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
+    secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
     proxy: true,
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: { secure: true }
 }
 
 if (app.get('env') === 'production') {
     app.set('trust proxy', 1) // trust first proxy
-    session.cookie.secure = true // serve secure cookies
     API_PORT = 4267;
 } else {
     API_PORT = 3000;
 }
 
-// app.use(session(sess))
+app.use(session(sess))
 
 var routes = require("./server/routes.js")(app);
 
